70178f1575f6fd054f3470b96c3c67fd99b46d9b
SabrinaAEH <sabrina.aeh@gmail.com>
2024-05-09 16:23:46 +0200

    commit avec profil pic ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :description])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :description])
   end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
+  end
+
 end
